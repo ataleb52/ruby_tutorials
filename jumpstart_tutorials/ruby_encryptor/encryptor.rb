fe46ce511efba8ed619c346e22981773a2a60f5f
Ahmad Taleb <ataleb52@gmail.com>
2013-05-18 12:23:58 -0700

    reworking the problem to better understand the second method

--- jumpstart_tutorials/ruby_encryptor/encryptor.rb
+++ jumpstart_tutorials/ruby_encryptor/encryptor.rb
@@ -1,4 +1,5 @@
 class Encryptor
+	
 	def cipher
 	{'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q', 'e' => 'r',
 	 'f' => 's', 'g' => 't', 'h' => 'u', 'i' => 'v', 'j' => 'w',
@@ -13,16 +14,4 @@ class Encryptor
 		cipher[lowercase_letter]
 	end
 
-	def encrypt(string)
-		# 1. Cut the input string into letter
-		letters = string.split("")
-		# 2. Encrypt those letter one at a time, gather the results
-		results = []
-		letters.each do |letter|
-			encrypted_letter = encrypt_letter(letter)
-			results.push(encrypted_letter)
-		end
-		# 3. Join th results back together in one string
-		results.join
-	end
 end
